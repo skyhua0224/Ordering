053e61b6a33adb362e8ff300c89068e57eb0e517
skyhua0224 <dev@sky-hua.xyz>
2024-03-19 19:53:53 +0800

    test windows support

--- payment.c
+++ payment.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
-#include <unistd.h>
 #include "payment.h"
 #include "color.h"
 #include "receipt.h"  // 包含 receipt.h 文件
+
+#ifdef _WIN32
+    #include <windows.h>
+    #define sleep(x) Sleep(1000 * (x))
+#elif _POSIX_C_SOURCE >= 199309L
+    #include <time.h>   // for nanosleep
+    void sleep(int sec) {
+        struct timespec ts;
+        ts.tv_sec = sec;
+        ts.tv_nsec = 0;
+        nanosleep(&ts, NULL);
+    }
+#else
+    #include <unistd.h> // for sleep
+#endif
 
 void askPrintReceipt(int tableNumber, double totalAmount, Dish* dishes) {
     // 提示用户是否打印小票
