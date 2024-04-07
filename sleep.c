7d584edc5bbd7aa042dfe2ed5da1ca7373237971
skyhua0224 <dev@sky-hua.xyz>
2024-04-07 12:14:06 +0800

    [modify] Update sleep.c and sleep.h to improve readability and add comments

--- sleep.c
+++ sleep.c
@@ -1,10 +1,12 @@
-#include "sleep.h"
+#include "sleep.h"  // 包含 sleep.h 文件，用于声明 sleep 函数
 
-#ifdef _POSIX_C_SOURCE >= 199309L
+// 如果定义了 _POSIX_C_SOURCE 并且其值大于或等于 199309L，那么就定义 sleep 函数
+#ifdef _POSIX_C_SOURCE //>= 199309L
+// sleep 函数，该函数接受一个参数 sec，表示要暂停的秒数
 void sleep(int sec) {
-    struct timespec ts;
-    ts.tv_sec = sec;
-    ts.tv_nsec = 0;
-    nanosleep(&ts, NULL);
+    struct timespec ts;  // 定义一个 timespec 结构体变量 ts，用于表示时间
+    ts.tv_sec = sec;  // 设置 ts 的秒数为 sec
+    ts.tv_nsec = 0;  // 设置 ts 的纳秒数为 0
+    nanosleep(&ts, NULL);  // 调用 nanosleep 函数，使程序暂停 sec 秒
 }
 #endif
--- sleep.h
+++ sleep.h
@@ -1,15 +1,15 @@
-// sleep.h
-#ifndef SLEEP_H
-#define SLEEP_H
+// sleep.h 文件，提供了跨平台的 sleep 函数
+#ifndef SLEEP_H  // 如果没有定义 SLEEP_H
+#define SLEEP_H  // 定义 SLEEP_H
 
-#ifdef _WIN32
-    #include <windows.h>
-    #define sleep(x) Sleep(1000 * (x))
-#elif _POSIX_C_SOURCE >= 199309L
-    #include <time.h>   // for nanosleep
-    void sleep(int sec);  // Only declare the function here
-#else
-    #include <unistd.h> // for sleep
+#ifdef _WIN32  // 如果是在 Windows 平台
+    #include <windows.h>  // 包含 windows.h 文件，用于调用 Windows API
+    #define sleep(x) Sleep(1000 * (x))  // 定义 sleep 函数，使用 Windows API 的 Sleep 函数实现，参数 x 表示要暂停的秒数
+#elif _POSIX_C_SOURCE >= 199309L  // 如果是在支持 POSIX 的平台，并且支持高精度的 sleep
+    #include <time.h>   // 包含 time.h 文件，用于调用 nanosleep 函数
+    void sleep(int sec);  // 只在这里声明 sleep 函数，具体的实现在 sleep.c 文件中
+#else  // 如果是在其他平台
+    #include <unistd.h> // 包含 unistd.h 文件，用于调用 sleep 函数
 #endif
 
-#endif // SLEEP_H
+#endif // 结束 #ifndef 指令
