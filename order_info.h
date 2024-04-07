f7fa902b5ef207b7b49cb730fd7159409b2e869d
skyhua0224 <dev@sky-hua.xyz>
2024-04-07 11:48:53 +0800

    [add] Refactor order_info.h to improve readability and add comments

--- order_info.h
+++ order_info.h
@@ -1,6 +1,7 @@
-#ifndef ORDER_INFO_H
-#define ORDER_INFO_H
+#ifndef ORDER_INFO_H  // 如果没有定义 ORDER_INFO_H
+#define ORDER_INFO_H  // 定义 ORDER_INFO_H
 
+// 声明 saveOrderInfo 函数，该函数接受四个参数：桌号、人数、订单数和总金额
 void saveOrderInfo(int tableNumber, int peopleNumber, int orderCount, double totalAmount);
 
-#endif
+#endif  // 结束 #ifndef 指令
