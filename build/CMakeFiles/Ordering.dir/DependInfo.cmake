0d3073b94f666d7e44645181e890104f3a34391c
skyhua0224 <dev@sky-hua.xyz>
2024-04-08 21:05:57 +0800

    [modify] Use CMake as our project manager

---
+++ build/CMakeFiles/Ordering.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/skyhua/Library/Mobile Documents/com~apple~CloudDocs/Coding/Project/Ordering/adminOrder.c" "CMakeFiles/Ordering.dir/adminOrder.c.o" "gcc" "CMakeFiles/Ordering.dir/adminOrder.c.o.d"
+  "/Users/skyhua/Library/Mobile Documents/com~apple~CloudDocs/Coding/Project/Ordering/admin_menu.c" "CMakeFiles/Ordering.dir/admin_menu.c.o" "gcc" "CMakeFiles/Ordering.dir/admin_menu.c.o.d"
+  "/Users/skyhua/Library/Mobile Documents/com~apple~CloudDocs/Coding/Project/Ordering/customer_menu.c" "CMakeFiles/Ordering.dir/customer_menu.c.o" "gcc" "CMakeFiles/Ordering.dir/customer_menu.c.o.d"
+  "/Users/skyhua/Library/Mobile Documents/com~apple~CloudDocs/Coding/Project/Ordering/dish.c" "CMakeFiles/Ordering.dir/dish.c.o" "gcc" "CMakeFiles/Ordering.dir/dish.c.o.d"
+  "/Users/skyhua/Library/Mobile Documents/com~apple~CloudDocs/Coding/Project/Ordering/main.c" "CMakeFiles/Ordering.dir/main.c.o" "gcc" "CMakeFiles/Ordering.dir/main.c.o.d"
+  "/Users/skyhua/Library/Mobile Documents/com~apple~CloudDocs/Coding/Project/Ordering/manageTable.c" "CMakeFiles/Ordering.dir/manageTable.c.o" "gcc" "CMakeFiles/Ordering.dir/manageTable.c.o.d"
+  "/Users/skyhua/Library/Mobile Documents/com~apple~CloudDocs/Coding/Project/Ordering/order.c" "CMakeFiles/Ordering.dir/order.c.o" "gcc" "CMakeFiles/Ordering.dir/order.c.o.d"
+  "/Users/skyhua/Library/Mobile Documents/com~apple~CloudDocs/Coding/Project/Ordering/order_info.c" "CMakeFiles/Ordering.dir/order_info.c.o" "gcc" "CMakeFiles/Ordering.dir/order_info.c.o.d"
+  "/Users/skyhua/Library/Mobile Documents/com~apple~CloudDocs/Coding/Project/Ordering/payment.c" "CMakeFiles/Ordering.dir/payment.c.o" "gcc" "CMakeFiles/Ordering.dir/payment.c.o.d"
+  "/Users/skyhua/Library/Mobile Documents/com~apple~CloudDocs/Coding/Project/Ordering/receipt.c" "CMakeFiles/Ordering.dir/receipt.c.o" "gcc" "CMakeFiles/Ordering.dir/receipt.c.o.d"
+  "/Users/skyhua/Library/Mobile Documents/com~apple~CloudDocs/Coding/Project/Ordering/table.c" "CMakeFiles/Ordering.dir/table.c.o" "gcc" "CMakeFiles/Ordering.dir/table.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Ordering.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Ordering.
+# This may be replaced when dependencies are built.
